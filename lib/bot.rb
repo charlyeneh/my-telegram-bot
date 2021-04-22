8389b34a848d15a37ff4c478979b36fa9f16696a
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-04-22 23:48:14 +0100

    fic code lrngth issues

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,5 +1,3 @@
-# rubocop:disable Layout/LineLength
-
 require 'telegram/bot'
 require_relative 'motivate'
 require_relative 'joke'
@@ -7,37 +5,27 @@ require_relative 'joke'
 class Bot
   def initialize
     token = '1711010278:AAFESksDfGU8KhvcZaPyM7ytQLqMjzt-SOs'
-
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         case message.text
         when '/start'
-
-          bot.api.send_message(chat_id: message.chat.id,
-                               text: "Hello, #{message.from.first_name} , welcome to Cenech Services Channel Bot. The chat bot is to keep you motivated and entertained. Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quote everytime you request for it or /joke to get a joke everytime you request for it")
-
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} ,
+                            welcome to Cenech Services Channel Bot. The chat bot is to keep you motivated")
         when '/stop'
-
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
         when '/motivate'
           values = Motivate.new
           value = values.select_random
           bot.api.send_message(chat_id: message.chat.id, text: (value['text']).to_s, date: message.date)
-
         when '/joke'
           values = Joke.new
           value = values.make_the_request
-
           bot.api.send_message(chat_id: message.chat.id, text: (value['joke']).to_s, date: message.date)
-
         else
-
-          bot.api.send_message(chat_id: message.chat.id,
-                               text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
+          bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name},
+                                you need to use /start,  /stop , /motivate or /joke")
         end
       end
     end
   end
 end
-
-# rubocop:enable Layout/LineLength
